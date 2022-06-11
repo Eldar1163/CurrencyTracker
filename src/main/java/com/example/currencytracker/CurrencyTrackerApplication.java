2fe280621cda021d25255bb1fb9006d31c0f14e8
Student <gusejnov_eldar@mail.ru>
2022-06-11 16:48:50 +0300

    Feign dependencies added

--- src/main/java/com/example/currencytracker/CurrencyTrackerApplication.java
+++ src/main/java/com/example/currencytracker/CurrencyTrackerApplication.java
@@ -2,8 +2,10 @@ package com.example.currencytracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyTrackerApplication {
 
     public static void main(String[] args) {
